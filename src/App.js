2dbf52be7bbfbfe9b905ac99e4a2785f39d41d4e
dragosbus <dragosbusuioc94@gmail.com>
2018-10-28 13:23:28 +0100

    added the card flipp animation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+export default function App() {
+  const [back, setBack] = useState(false);
+  const onChangeSide = () => {
+    setTimeout(() => {
+      document.querySelector('.card').classList.add('card-flip');
+      setTimeout(() => {
+        setBack(!back);
+        document.querySelector('.card').classList.remove('card-flip');
+      }, 500);
+    }, 1000);
+  };
+  return (
+    <div>
+      <div className="card" onClick={onChangeSide}>
+        {back ? 'back' : 'front'}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default App;
+render(<App />, document.getElementById('root'));
